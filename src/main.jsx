862846108e8c40e343e75470c5dc2bc0cac47734
Chirag Bhatia <chiragbhatia.here2005@gmail.com>
2025-05-18 13:08:04 +0530

    ui library setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
 import { BrowserRouter } from "react-router";
 import { Routes, Route } from "react-router";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path='/sign-in' element={<SignIn />} /> */}
-        <Route path='/' element={<App />} />
-      </Routes>
-      
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/sign-in' element={<SignIn />} /> */}
+          <Route path='/' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+
   </StrictMode>,
 )
